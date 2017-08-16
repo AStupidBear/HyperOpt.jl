96f2c106ec87a8d0d7366a7f9d091bfc7e91a79f
Yao Lu <luyaocns@gmail.com>
2017-08-16 13:49:48 +0800

    ispath(Pkg.dir(pkg))

--- deps/build.jl
+++ deps/build.jl
@@ -1,5 +1,5 @@
 for pkg in ["Utils"]
-    Pkg.installed(pkg) !== nothing && (Pkg.checkout(pkg); continue)
+    ispath(Pkg.dir(pkg)) && (Pkg.checkout(pkg); continue)
     Pkg.clone("https://github.com/AStupidBear/$pkg.jl.git")
     Pkg.build(pkg)
 end
